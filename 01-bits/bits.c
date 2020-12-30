f99ad8fdad9b1f84943e3d589d4efc76efc9ba63
Leonardo Ventura <leonardo.venturacl@gmail.com>
2020-12-30 13:44:27 -0300

    Atualiza primeiro laboratorio para 2020.1

--- 01-bits/bits.c
+++ 01-bits/bits.c
@@ -11,7 +11,6 @@
  * for pedido no enunciado.
  *
  * Todas as operações permitidas serão especificadas em cada questão.
- * Não está permitido o uso de if-else, for, while, switch, etc.
  *
  * Forma de avaliação:
  *      - Quantas operações o aluno utlizou para realizar a tarefa (se está dentro da quantidade aceitável)
@@ -25,8 +24,6 @@
  *      DRE: <DRE>
  *      versão do GCC utilizada: XX.XX.XX
  *
- * Se descobrir algo errado, inesperado ou engraçado com o código,
- * entre em contato com o monitor!
  */
 
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -37,80 +34,61 @@
 #include <stdio.h>
 #include <stdint.h>
 
-/*
- * Diferente sem !=
+/* Número não é zero
  *      Permitido:
- *          Operações bitwise: ~ & ^ | ! << >>
+ *          Operações: ~ & ^ | ! << >>
  *
- *      Número máximo de operações: 6
- *      Monitor: 3
+ *      Número máximo de operações: 3
+ *      Monitor: 2
  *
- *      Retorna 1 se x != y, 0 caso contrário
+ *      Retorna 1 se x é diferente 0, retorna 0 caso contrário
  *
  *      Exemplo:
- *          ehDiferente(10, 10) -> 0
- *          ehDiferente(16, 8) -> 1
+ *          naoEhZero(0) -> 0
+ *          naoEhZero(7) -> 1
  */
-int32_t ehDiferente(int32_t x, int32_t y) {
+int32_t naoEhZero(int32_t x) {
+    return -1;
+}
+
+/* Número é par ou não
+ *      Permitido:
+ *          Operações: ~ & ^ | ! << >>
+ *
+ *      Número máximo de operações: 3
+ *      Monitor: 2
+ *
+ *      Retorna 1 se x é par, retorna 0 caso contrário
+ *
+ *      Exemplo:
+ *          ehPar(0) -> 1
+ *          ehPar(2) -> 1
+ *          ehPar(7) -> 0
+ */
+int32_t ehPar(int32_t x) {
     return -1;
 }
 
 /*
- * Número é zero ou não
+ * Módulo 8
  *      Permitido:
  *          Operações: ~ & ^ | ! << >>
  *
  *      Número máximo de operações: 3
  *      Monitor: 1
  *
- *      Retorna 1 se x é 0, retorna 0 caso contrário
+ *      Retorna x % 8
  *
  *      Exemplo:
- *          ehZero(0) -> 1
- *          ehZero(7) -> 0
+ *          mod8(1) -> 1
+ *          mod8(7) -> 7
+ *          mod8(10) -> 2
  */
-int32_t ehZero(int32_t x) {
+int32_t mod8(int32_t x) {
     return -1;
 }
 
-/*
- * Número é ímpar ou não
- *      Permitido:
- *          Operações: ~ & ^ | ! << >>
- *
- *      Número máximo de operações: 4
- *      Monitor: 1
- *
- *      Retorna 1 se x é ímpar, retorna 0 caso contrário
- *
- *      Exemplo:
- *          ehImpar(0) -> 0
- *          ehImpar(7) -> 1
- */
-int32_t ehImpar(int32_t x) {
-    return -1;
-}
-
-/*
- * Módulo 4
- *      Permitido:
- *          Operações: ~ & ^ | ! << >>
- *
- *      Número máximo de operações: 3
- *      Monitor: 1
- *
- *      Retorna x % 4
- *
- *      Exemplo:
- *          mod4(1) -> 1
- *          mod4(7) -> 3
- */
-int32_t mod4(int32_t x) {
-    return -1;
-}
-
-/*
- * Número positivo ou não
+/* Número positivo ou não
  *      Permitido:
  *          Operações: ~ & ^ | ! << >>
  *
@@ -127,12 +105,11 @@ int32_t ehPositivo(int32_t x) {
     return -1;
 }
 
-/*
- * Negativo sem -
+/* Negativo sem -
  *      Permitido:
  *          Operações: ~ & ^ | ! << >> +
  *
- *      Número máximo de operações: 4
+ *      Número máximo de operações: 5
  *      Monitor: 2
  *
  *      Retorna -x
@@ -145,40 +122,56 @@ int32_t negativo(int32_t x) {
     return -1;
 }
 
-/*
- * Implementação do | usando bitwise
+/* Implementação do & usando bitwise
  *      Permitido:
- *          Operações: ~ & ^ ! << >>
+ *          Operações: ~ ^ | ! << >>
  *
  *      Número máximo de operações: 7
  *      Monitor: 4
  *
- *      Retorna x | y
+ *      Retorna x & y
  *
  *      Exemplo:
- *          bitwiseOr(1, 2) -> 3
- *              1 | 10 -> 11
- *          bitwiseOr(3, 9) -> 11
- *              11 | 1001 -> 1011
+ *          bitwiseAnd(1, 2) -> 0
+ *              01 & 10 -> 00
+ *          bitwiseAnd(3, 11) -> 3
+ *              11 & 1011 -> 0011
  */
-int32_t bitwiseOr(int32_t x, int32_t y) {
+int32_t bitwiseAnd(int32_t x, int32_t y) {
+    return -1;
+}
+
+/* Igual sem ==
+ *      Permitido:
+ *          Operações bitwise: ~ & ^ | << >>
+ *
+ *      Número máximo de operações: 3
+ *      Monitor: 2
+ *
+ *      Retorna 1 se x == y, 0 caso contrário
+ *
+ *      Exemplo:
+ *          ehIgual(10, 10) -> 1
+ *          ehIgual(16, 8) -> 0
+ */
+int32_t ehIgual(int32_t x, int32_t y) {
     return -1;
 }
 
 /*
- * Multiplicação por 6
+ * Multiplicação por 7
  *      Permitido:
- *          Operações: << >> | ! & +
+ *          Operações: << >> | ! & + -
  *
- *      Número máximo de operações: 3
- *      Monitor: 3
+ *      Número máximo de operações: 4
+ *      Monitor: 2
  *
- *      Retorna x multiplicado por 6
+ *      Retorna x multiplicado por 7
  *
  *      Exemplo:
- *          mult6(36) -> 216
+ *          mult7(7) -> 49
  */
-int32_t mult6(int32_t x) {
+int32_t mult7(int32_t x) {
     return -1;
 }
 
@@ -187,8 +180,8 @@ int32_t mult6(int32_t x) {
  *      Permitido:
  *          Operações bitwise: << >> | & + -
  *
- *      Número máximo de operações: 10
- *      Monitor: 6
+ *      Número máximo de operações: 6
+ *      Monitor: 2
  *
  *      Retorna o valor do bit na posição p no inteiro x
  *      Valor de retorno pode ser apenas 0 ou 1
@@ -241,9 +234,9 @@ int32_t byteEmP(int32_t x, uint8_t p) {
 /*
  * Negação lógica sem !
  *      Permitido:
- *          Operações: << >> | & + ~
+ *          Operações: << >> | & +
  *
- *      Número máximo de operações: 9
+ *      Número máximo de operações: 15
  *      Monitor: 5
  *
  *      Retorna 1 se x == 0, retorna 0 caso contrário
@@ -258,77 +251,59 @@ int32_t negacaoLogica(int32_t x) {
 }
 
 void teste(int32_t saida, int32_t esperado) {
-    static uint8_t count = 0;
-    count++;
+    static uint8_t test_number = 0;
+    test_number++;
     if(saida == esperado)
         printf(ANSI_COLOR_GREEN "PASSOU! Saída: %-10d\t Esperado: %-10d\n" ANSI_COLOR_RESET,
             saida, esperado);
 
     else
         printf(ANSI_COLOR_RED "%d: FALHOU! Saída: %-10d\t Esperado: %-10d\n" ANSI_COLOR_RESET,
-            count, saida, esperado);
+            test_number, saida, esperado);
 }
 
 int main() {
     puts(ANSI_COLOR_BLUE "Primeiro lab - bits" ANSI_COLOR_RESET);
     puts("");
 
-    puts("Teste: ehDiferente");
-    teste(ehDiferente(1, 1), 0);                    // 1 NÃO É diferente de 1, deve retornar 0
-    teste(ehDiferente(7, 13), 1);                   // 7 É diferente de 13, deve retornar 1
-    teste(ehDiferente(1, -2147483648), 1);
-    teste(ehDiferente(2147483647, -1), 1);
-    teste(ehDiferente(1, -1), 1);
-    teste(ehDiferente(2147483647, 2147483647), 0);
-    teste(ehDiferente(0,0), 0);
-    teste(ehDiferente(123, 124), 1);
-    teste(ehDiferente(1073741823, 1073741823), 0);
-    teste(ehDiferente(1073741823, 1073741822), 1);
-    teste(ehDiferente(1073741823, -1073741824), 1);
-    teste(ehDiferente(-1073741824, -1073741824), 0);
-    teste(ehDiferente(-1073741824, -2147483648), 1);
-    teste(ehDiferente(1024, 1000), 1);
-    teste(ehDiferente(2048, -2048), 1);
-    teste(ehDiferente(0xFF, -0xFF), 1);
-    puts("");
-
-    puts("Teste: ehImpar");
-    teste(ehImpar(2), 0);       // 2 NÃO É impar, deve retornar 0
-    teste(ehImpar(1), 1);       // 1 É impar, deve retornar 1
-    teste(ehImpar(3), 1);
-    teste(ehImpar(13), 1);
-    teste(ehImpar(100), 0);
-    teste(ehImpar(125), 1);
-    teste(ehImpar(1024), 0);
-    teste(ehImpar(2019), 1);
-    teste(ehImpar(2020), 0);
-    teste(ehImpar(-1), 1);
-    teste(ehImpar(-27), 1);
-    teste(ehImpar(-1073741825), 1);
-    teste(ehImpar(1073741824), 0);
-    teste(ehImpar(2147483647), 1);
-    teste(ehImpar(-2147483648), 0);
-    teste(ehImpar(0), 0);
-    puts("");
-
-    puts("Teste: mod4");
-    teste(mod4(4), 0);       // 4 % 4 = 0
-    teste(mod4(7), 3);       // 7 % 4 = 3
-    teste(mod4(16), 0);
-    teste(mod4(2049), 1);
-    teste(mod4(404), 0);
-    teste(mod4(8197), 1);
-    puts("");
-
     puts("Teste: ehZero");
-    teste(ehZero(1), 0);
-    teste(ehZero(10000), 0);
-    teste(ehZero(0), 1);
-    teste(ehZero(2147483647), 0);
-    teste(ehZero(0), 1);
-    teste(ehZero(-0), 1);
-    teste(ehZero(0x0), 1);
-    teste(ehZero(0xFF), 0);
+    teste(naoEhZero(0), 0);
+    teste(naoEhZero(1), 1);
+    teste(naoEhZero(1024), 1);
+    teste(naoEhZero(-1), 1);
+    teste(naoEhZero(-2), 1);
+    teste(naoEhZero(-2147483648), 1);
+    teste(naoEhZero(2147483647), 1);
+    puts("");
+
+    puts("Teste: ehPar");
+    teste(ehPar(2), 1);
+    teste(ehPar(1), 0);
+    teste(ehPar(3), 0);
+    teste(ehPar(13), 0);
+    teste(ehPar(100), 1);
+    teste(ehPar(125), 0);
+    teste(ehPar(1024), 1);
+    teste(ehPar(2019), 0);
+    teste(ehPar(2020), 1);
+    teste(ehPar(-1), 0);
+    teste(ehPar(-27), 0);
+    teste(ehPar(-1073741825), 0);
+    teste(ehPar(1073741824), 1);
+    teste(ehPar(2147483647), 0);
+    teste(ehPar(-2147483648), 1);
+    teste(ehPar(0), 1);
+    puts("");
+
+    puts("Teste: mod8");
+    teste(mod8(0), 0);
+    teste(mod8(4), 4);
+    teste(mod8(7), 7);
+    teste(mod8(8), 0);
+    teste(mod8(-1), 7);
+    teste(mod8(-8), 0);
+    teste(mod8(2147483647), 7);
+    teste(mod8(-2147483648), 0);
     puts("");
 
     puts("Teste: ehPositivo");
@@ -339,21 +314,43 @@ int main() {
     teste(ehPositivo(-2147483648), 0);
     teste(ehPositivo(-2147483648/2), 0);
     teste(ehPositivo(2147483647), 1);
-    teste(ehPositivo(2147483648), 0);
     puts("");
 
     puts("Teste: negativo");
     teste(negativo(0), 0);
-    teste(negativo(-12), 12);
-    teste(negativo(-20), 20);
-    teste(negativo(1109563935), -1109563935);
+    teste(negativo(1), -1);
+    teste(negativo(-1), 1);
+    teste(negativo(2147483647), -2147483647);
+    teste(negativo(-2147483647), 2147483647);
+    teste(negativo(-2147483648), 2147483648);
     puts("");
 
-    puts("Teste: bitwiseOr");
-    teste(bitwiseOr(48, 80), 112);
-    teste(bitwiseOr(448, 24), 472);
-    teste(bitwiseOr(106, 42), 106);
-    teste(bitwiseOr(832, 625), 881);
+    puts("Teste: bitwiseAnd");
+    teste(bitwiseAnd(1, 3), 1);
+    teste(bitwiseAnd(-1, 0), 0);
+    teste(bitwiseAnd(-1, 0x7FFFFFFF), 0x7FFFFFFF);
+    teste(bitwiseAnd(0b0100, 0b1100), 0b0100);
+    puts("");
+
+    puts("Teste: ehIgual");
+    teste(ehIgual(1,1), 1);
+    teste(ehIgual(1,0), 0);
+    teste(ehIgual(0,1), 0);
+    teste(ehIgual(-1,1), 0);
+    teste(ehIgual(-1,-1), 1);
+    teste(ehIgual(2147483647,-1), 0);
+    teste(ehIgual(2147483647,-2147483647), 0);
+    teste(ehIgual(2147483647,-2147483648), 0);
+    teste(ehIgual(2147483647,-2147483648), 0);
+    puts("");
+
+    puts("Teste: mult7");
+    teste(mult7(1), 7);
+    teste(mult7(7), 49);
+    teste(mult7(3), 21);
+    teste(mult7(-1), -7);
+    teste(mult7(-306783378), -2147483646);
+    teste(mult7(306783378), 2147483646);
     puts("");
 
     puts("Teste: bitEmP");
@@ -365,7 +362,7 @@ int main() {
     teste(bitEmP(-4194305, 22), 0);
     teste(bitEmP(9, 3), 1);
     teste(bitEmP(16, 3), 0);
-    teste(bitEmP(24, 2), 0);
+    teste(bitEmP(0x1 << 5, 4), 0);
     teste(bitEmP(0x1 << 31, 31), 1);
     teste(bitEmP(-1073741825, 30), 0);
     teste(bitEmP(-1073741825, 31), 1);
@@ -381,15 +378,6 @@ int main() {
     teste(byteEmP(0x12345678, 0), 0x78);   // retorna 0x78
     teste(byteEmP(0x321, 1), 0x03);        // retorna 0x03
     teste(byteEmP(0x321, 0), 0x21);        // retorna 0x21
-    puts("");
-
-    puts("Teste: mult6");
-    teste(mult6(1), 6);
-    teste(mult6(6), 36);
-    teste(mult6(3), 18);
-    teste(mult6(-1), -6);
-    teste(mult6(-357913391), -2147480346);
-    teste(mult6(357913941), 2147483646);
     puts("");
 
     puts("Teste: negacaoLogica");
